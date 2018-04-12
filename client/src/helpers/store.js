62ccfa894a1b44e51636f97062c26836bbfe54ea
Chintogtokh Batbold <bchintogtokh@gmail.com>
2018-04-12 04:53:39 +0000

    fix for redux devtools on prod

--- client/src/helpers/store.js
+++ client/src/helpers/store.js
@@ -5,12 +5,11 @@ import rootReducer from '../reducers';
 
 //const loggerMiddleware = createLogger();
 
-export const store = createStore(
-    rootReducer,
-    compose(
-    	applyMiddleware(
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
+	applyMiddleware(
         	thunkMiddleware
     	),
-    	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+	)
 );
