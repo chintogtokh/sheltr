0fc04d236ac86a2b11791be6199ca6d62ff5e97c
Chintogtokh Batbold <bchintogtokh@gmail.com>
2018-05-19 05:40:18 +0000

    fix for #22

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
-import tram from '../../files/tram.svg';
 
 import './Footer.css';
 
 class Footer extends Component {
 
-  state = {
-    users:[]
-  }
-
   render() {
     return (
-      <div>
-        <footer className="container">
+        <footer>
           <p>
           <i className="fas fa-code"></i> with <i className="fas fa-beer"></i> in Melbourne by team saladandscrambedeggs &middot; &nbsp;
           <HashLink to="/">home</HashLink>&nbsp;&middot;&nbsp;
@@ -22,12 +16,8 @@ class Footer extends Component {
           &nbsp;&middot;&nbsp;
           <Link to="/contact">contact</Link>&nbsp;&middot;&nbsp;
           <Link to="/credits">credits</Link>
-          <span className="tram-container">
-            <img src={tram} alt="Tram" title="Ding!"/>
-          </span>
           </p>
         </footer>
-      </div>
     );
   }
 }
